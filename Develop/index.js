0e58bed0a50e2056e65cbb9c1c5819ed7460e6ff
EWager <ethanbwager@gmail.com>
2020-11-17 12:31:24 -0500

    innit

---
+++ Develop/index.js
@@ -0,0 +1,16 @@
+//import inquirer
+let inquirer = require("inquirer");
+
+//import fs
+let fs = require("fs");
+// array of questions for user
+const questions = [];
+
+// function to write README file
+function writeToFile(fileName, data) {}
+
+// function to initialize program
+function init() {}
+
+// function call to initialize program
+init();
